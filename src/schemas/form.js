283ccf1371fbeb78bb4bd4c4c9dae2355879c31d
Artem Yankin <yankin.artem@outlook.com>
2017-06-05 14:32:21 +0500

    Removed block parent calculating

--- src/schemas/form.js
+++ src/schemas/form.js
@@ -1,30 +1,8 @@
 import { schema } from 'normalizr';
 
-const getPath = (entity) => (
-    Object.prototype.toString.call(entity.path) === "[object Array]"
-        ? entity.path.concat(entity.id)
-        : [entity.path].concat(entity.id)
-);
-
-const block = new schema.Entity("blocks", {}, {
-    processStrategy: (entity, parent, key) => ({
-        ...entity,
-        path: getPath(parent)
-    })
-});
-const subsection = new schema.Entity("subsections", { blocks: [ block ] }, {
-    processStrategy: (entity, parent, key) => ({
-        ...entity,
-        path: getPath(parent)
-
-    })
-});
-const section = new schema.Entity("sections", { subsections: [ subsection ] }, {
-    processStrategy: (entity, parent, key) => ({
-        ...entity,
-        path: parent.id
-    })
-});
+const block = new schema.Entity("blocks");
+const subsection = new schema.Entity("subsections", { blocks: [ block ] });
+const section = new schema.Entity("sections", { subsections: [ subsection ] });
 const full_schema = new schema.Object({ sections: [ section ] });
 
 export default full_schema
